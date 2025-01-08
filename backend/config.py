99022202da900df236d38093e8729b52011e30cc
miguelpena-bigdata-dev <miguel674@revature.net>
2025-01-08 11:59:09 -0700

    configured environment for flask-sqlalchemy backend

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv('./.env')
+
+FLASK_DEBUG = os.environ.get('FLASK_DEBUG', False)
+FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT', 9000)
+FLASK_RUN_HOST = os.environ.get('FLASK_RUN_HOST', "0.0.0.0")
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
